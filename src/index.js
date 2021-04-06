09b8e82f09374871101e88e61f92b39ab34e4ded
axlopez8 <axlopez0038@gmail.comm>
2021-04-05 18:58:11 -0600

    implementacion de cors
    se utilizaron cors abiertas para poder ser utilizadas

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 const express = require('express');
+const cors=require('cors');
 const app = express();
+
 
 //Configuracion
 app.set('port', process.env.PORT || 3000);
 
 //Middlewares (funciones)
 app.use(express.json());
+app.use(cors());
 //Rutas (URL)
 app.use(require('./rutas/rutas'));
 //Empezando el servidor
